249d2b503da4e0c048b12ab59536af7d588b18aa
Godstime Peculiar <godstimepeculiar7@gmail.com>
2026-07-16 13:50:05 +0100

    made some changes

--- seeds/seedSwallowProducts.js
+++ seeds/seedSwallowProducts.js
@@ -4,7 +4,6 @@ const SwallowProduct = require('../models/swallowProducts');
 
 const swallow = [
     {
-        id: crypto.randomUUID(),
         name: "Egusi soup and Eba",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1Ym0DIny2Pce-DGT-IafP2mMf6BmROKKmnw&s",
         priceCents: 5000,
@@ -13,7 +12,6 @@ const swallow = [
 
 
     {
-        id: crypto.randomUUID(),
         name: "Okra soup and Semovita",
         image: "https://i.ytimg.com/vi/sziLuU3Yzso/maxresdefault.jpg",
         priceCents: 5000,
@@ -22,7 +20,6 @@ const swallow = [
 
 
     {
-        id: crypto.randomUUID(),
         name: "Edikang Ikong and Fufu",
         image: "https://static.wixstatic.com/media/6cadac_21e3d4783aa34e7383552874e393eb6a~mv2.jpeg/v1/fill/w_568,h_638,al_c,lg_1,q_85,enc_avif,quality_auto/6cadac_21e3d4783aa34e7383552874e393eb6a~mv2.jpeg",
         priceCents: 6000,
@@ -30,7 +27,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Afang soup with Goat meat",
         image: "https://feastandbeyond.co.uk/wp-content/uploads/2023/10/Afang-Soup-600x600.jpeg",
         priceCents: 7000,
@@ -38,7 +34,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Bitterleaf soup with Assorted meat",
         image: "https://wigmoretrading.com/wp-content/uploads/2024/08/Bitterleaf-Soup.jpg",
         priceCents: 6500,
@@ -46,7 +41,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: " Groundnut soup with Fish",
         image: "https://api.flavournetwork.ca/wp-content/uploads/2023/01/groundnut-soup-feat.jpg?w=3840&quality=75",
         priceCents: 6000,
@@ -56,7 +50,6 @@ const swallow = [
 
 
     {
-        id: crypto.randomUUID(),
         name: "Oha soup with Beef",
         image: "https://e4ma.kastechnet.com/wp-content/uploads/2021/09/Ora-Oha-Soup.jpg",
         priceCents: 7000,
@@ -64,7 +57,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Nsala soup with Catfish",
         image: "https://i.ytimg.com/vi/FB7d7WoRlwk/maxresdefault.jpg",
         priceCents: 7500,
@@ -75,7 +67,6 @@ const swallow = [
 
 
     {
-        id: crypto.randomUUID(),
         name: "Afia Efere with Chicken",
         image: "https://steemitimages.com/1280x0/https://steemitimages.com/DQmTLMkdWtb9GSx7GRtwp6YSZExEzY3Ld9MZsCZ5pCpJuo2/white-soup3.jpg",
         priceCents: 8000,
@@ -83,7 +74,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Ewedu soup with Assorted meat",
         image: "https://shopafricausa.com/cdn/shop/articles/abula-gbegiri-recipe-img-2_600x.jpg?v=1657717654",
         priceCents: 5500,
@@ -91,7 +81,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Vegetable soup with Fish",
         image: "https://thumbs.dreamstime.com/b/nigerian-vegetable-soup-fish-bush-meat-white-plate-afang-large-dinner-traditional-edikang-ikong-disht-348084862.jpg",
         priceCents: 6000,
@@ -99,7 +88,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Egusi pepper soup with Goat meat",
         image: "https://i.ytimg.com/vi/6nMzGv8bQAQ/maxresdefault.jpg",
         priceCents: 7000,
@@ -107,7 +95,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Banga soup with Catfish and meat",
         image: "https://www.myactivekitchen.com/wp-content/uploads/2015/03/niger-delta-banga-soup-recipe-img-10.jpg",
         priceCents: 7500,
@@ -115,7 +102,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Ofe Akwu with Chicken",
         image: "https://morielsoasis.ca/cdn/shop/files/rn-image_picker_lib_temp_646160ec-2b2b-47b3-a74f-923ed6b58e90.png?v=1729516761&width=1946",
         priceCents: 8000,
@@ -123,7 +109,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Atama soup",
         image: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/abak-atama-soup.jpg",
         priceCents: 6500,
@@ -131,7 +116,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Afang soup with Fish",
         image: "https://feastandbeyond.co.uk/wp-content/uploads/2023/10/Afang-Soup-600x600.jpeg",
         priceCents: 6000,
@@ -139,7 +123,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Edikang Ikong with Goat meat",
         image: "https://static.wixstatic.com/media/6cadac_21e3d4783aa34e7383552874e393eb6a~mv2.jpeg/v1/fill/w_568,h_638,al_c,lg_1,q_85,enc_avif,quality_auto/6cadac_21e3d4783aa34e7383552874e393eb6a~mv2.jpeg",
         priceCents: 7000,
@@ -147,7 +130,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Okra soup with Beef",
         image: " https://allnigerianfoods.com/wp-content/uploads/okro_soup_recipe.jpg",
         priceCents: 7500,
@@ -155,7 +137,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Egusi soup with Catfish",
         image: "https://allnigerianfoods.com/wp-content/uploads/egusi-catfish-soup1.jpg",
         priceCents: 8000,
@@ -163,7 +144,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Oha soup with Chicken",
         image: "https://www.avilauk.com/wp-content/uploads/2020/11/rsz_oha_soup_org.jpg",
         priceCents: 8500,
@@ -171,7 +151,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Banga soup with Assorted meat",
         image: "https://www.fmnfoods.com/wp-content/uploads/2020/07/banga_soup_-600.jpg",
         priceCents: 7000,
@@ -179,7 +158,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Nsala soup with Fish",
         image: "https://i.ytimg.com/vi/FB7d7WoRlwk/maxresdefault.jpg",
         priceCents: 7500,
@@ -187,7 +165,6 @@ const swallow = [
     },
 
     {
-        id: crypto.randomUUID(),
         name: "Afia Efere with Goat meat",
         image: "https://discoverakwaibom.com/wp-content/uploads/2024/05/afia-2.jpg",
         priceCents: 8000,
